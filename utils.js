d3f5e93cbed043c5080db87329b0721bdcc94b43
ahmed <127679016+br4o@users.noreply.github.com>
2026-03-05 19:19:35 +0200

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:19:21.739Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T17:19:33.648Z
+Commit: Resolve null pointer exception in user service
