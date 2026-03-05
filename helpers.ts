8dcb8d72180fe887e4f4a8b31420a22b4ef5f2f9
ahmed <127679016+br4o@users.noreply.github.com>
2026-03-05 19:21:55 +0200

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:19:54.261Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T17:21:53.832Z
+Commit: Fix broken links in documentation
